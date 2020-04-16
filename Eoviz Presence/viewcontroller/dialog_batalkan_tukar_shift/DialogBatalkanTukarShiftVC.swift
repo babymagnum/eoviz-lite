d7cc0ceab13577a94b8149d8f5b64290a95a52d1
babymagnum <flaminglassoo1996@gmail.com>
2020-04-16 10:09:33 +0700

    fixing some bug

---
+++ Eoviz Presence/viewcontroller/dialog_batalkan_tukar_shift/DialogBatalkanTukarShiftVC.swift
@@ -0,0 +1,19 @@
+//
+//  DialogBatalkanTukarShiftVC.swift
+//  Eoviz Presence
+//
+//  Created by Arief Zainuri on 16/04/20.
+//  Copyright © 2020 Gama Techno. All rights reserved.
+//
+
+import UIKit
+
+class DialogBatalkanTukarShiftVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
